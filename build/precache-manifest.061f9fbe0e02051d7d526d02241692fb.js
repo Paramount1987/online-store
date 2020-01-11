3a86e40ccd08ecb67a0301ec27e4e7a1edb97576
Artem Petrov <Paramount7891@gmail.com>
2020-01-11 14:56:43 +0300

    build

---
+++ build/precache-manifest.061f9fbe0e02051d7d526d02241692fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a6d213fda1fcc606ff648b1157a408",
+    "url": "/online-store/build/index.html"
+  },
+  {
+    "revision": "484f0743a4aa5cc5dbbd",
+    "url": "/online-store/build/static/css/main.37c0224b.chunk.css"
+  },
+  {
+    "revision": "6a107a4d4ee319cf3df9",
+    "url": "/online-store/build/static/js/2.b29275ce.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/online-store/build/static/js/2.b29275ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "484f0743a4aa5cc5dbbd",
+    "url": "/online-store/build/static/js/main.3812a711.chunk.js"
+  },
+  {
+    "revision": "d7152651931a1978f46f",
+    "url": "/online-store/build/static/js/runtime-main.e9afeb22.js"
+  }
+]);
